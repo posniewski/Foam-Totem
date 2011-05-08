9922aa4f7028c8ca9452e1a9f1759ca795c344ee
Shannon Posniewski <posniewski@gmail.com>
2011-05-07 18:56:13 -0700

    Added a couple more bits.

---
+++ html/daily/testfoam2.pl
@@ -0,0 +1,8 @@
+#!perl -w
+require Foam2;
+
+Foam2::UpdateHTML("/home/www/html/daily", 2011, 1);
+Foam2::UpdateHTML("/home/www/html/daily", 2011, 2);
+Foam2::UpdateHTML("/home/www/html/daily", 2011, 3);
+Foam2::UpdateHTML("/home/www/html/daily", 2011, 4);
+Foam2::UpdateHTML("/home/www/html/daily", 2011, 5);
